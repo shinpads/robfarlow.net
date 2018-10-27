f66cf0939b12167e991ea4fc1b814a888aceda9f
Rob <robfarlow@gmail.com>
2018-10-27 13:00:20 -0400

    new react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Home from './components/Home';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+          <Route path="/" component={Home} exact />
+      </Switch>
+    );
+  }
+}
+
+
+
+export default App;
