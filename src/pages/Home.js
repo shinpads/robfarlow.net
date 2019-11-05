39bc9e419ff27533229cd8e30aaab9a78052bf25
Rob <robfarlow@gmail.com>
2019-11-04 22:13:26 -0500

    updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,6 +17,7 @@ import Projects from '../components/Projects';
 import Games from '../components/Games';
 import Footer from '../components/Footer';
 import Spinner from '../components/Spinner';
+import CloudSvg from '../../public/cloud2.svg';
 
 class Home extends Component {
   constructor(props) {
@@ -36,13 +37,13 @@ class Home extends Component {
       window.isMobile = isMobile;
       this.setState({ isMobile });
     });
-    // window.addEventListener('scroll', () => {
-    //   const parent =document.getElementById('parallax-container');
-    //   const children = parent.querySelectorAll('#parallax-container>.parallax-layer');
-    //   for(let i = 0; i < children.length; i++) {
-    //     children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
-    //   }
-    // }, false);
+    window.addEventListener('scroll', () => {
+      const parent =document.getElementById('parallax-container');
+      const children = parent.querySelectorAll('#parallax-container>.parallax-layer');
+      for(let i = 0; i < children.length; i++) {
+        children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+      }
+    }, false);
     setTimeout(() => {
       this.setState({ loading: false });
     }, 2500);
@@ -50,47 +51,34 @@ class Home extends Component {
   render() {
     return (
       <div className="page-container">
-        {/*<div id="parallax-container">
+        <div id="parallax-container">
           <div className="parallax-layer">
-            <Sun style={{ top: '-5%', right: '0%' }} scale={0.4} />
-            <Mountain scale={0.5} style={{ top: '30%', left:'0%' }}/>
+            {/*<Sun style={{ top: '-5%', right: '0%' }} scale={0.4} /> */}
           </div>
           <div className="parallax-layer">
-            <Cloud style={{}} scale={2.3} />
-            <Cloud style={{ top: '5%', right: '30%' }} top={5} scale={2.8} />
-            <Cloud style={{ top: '15%', right: '10%' }} top={15} scale={2.5} />
-            <Cloud style={{ top: '8%', right: '60%' }} top={8} scale={2.2} />
+            <Mountain scale={0.75} style={{ top: '30%', left:'20%', opacity: '0.5' }}/>
           </div>
           <div className="parallax-layer">
-            <Building2 style={{ left: '20%' }} top={-15} scale={0.3} />
-            <Building1 style={{ left: '55%' }} top={-13} scale={0.4} />
-            <Building4 style={{ left: '65%' }} scale={0.7} />
-            <Building1 scale={0.5} top={-20} />
-            <Building2 style={{ right: '20%' }} top={-21} scale={0.42} />
-            <Building1 style={{ right: '55%' }} top={-18} scale={0.46} />
-            <Building4 style={{ right: '65%' }} top={10} scale={0.6} />
-            <Building1 style={{ left: '100%'}} scale={0.5} top={-20} />
+            <Cloud style={{}} scale={3.3} />
+            <Cloud style={{ top: '5%', right: '30%' }} top={5} scale={3.8} />
+            <Cloud style={{ top: '15%', right: '10%' }} top={15} scale={3.5} />
+            <Cloud style={{ top: '8%', right: '60%' }} top={8} scale={3.2} />
           </div>
           <div className="parallax-layer">
-            <Building3 scale={0.65} top={-30} style={{ left: '40%'}} />
-            <Building5 top={-50} left={0} scale={0.5} />
-            <Building2 style={{ left: '80%' }} top={-30} scale={0.4} />
+            <Mountain scale={1.2} style={{ top: '30%', left:'0%' }}/>
           </div>
           <div className="parallax-layer">
-            <div style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              width: '100%',
-              height: '100%',
-            }}>
-              <Menu />
-            </div>
+
           </div>
         </div>
-        <div style={{ height: '110vh' }} /> */}
-        <TitleSection />
-        {/*<About />
+        <div style={{ minHeight: '170vh' }} />
+        <div>
+          <object style={{ width: '100%', height: '100%' }} data={CloudSvg} type="image/svg+xml">
+            <img src={CloudSvg} />
+          </object>
+        </div>
+        {/*<TitleSection />
+        <About />
         <Projects />
         <Games />
         <Footer />*/}
